db221efafc7a09fb355fbe174d32f33adfae2ad5
jatosdigitalstudio <jatosajibuana@gmail.com>
2025-04-24 13:05:52 +0700

    final rev v4

--- src/components/Pages/Hero.jsx
+++ src/components/Pages/Hero.jsx
@@ -1,30 +1,13 @@
-// import HeroSlider from "../UI/HeroSlider";
-
-// export default function Hero() {
-//   return (
-//     <section className="relative flex h-screen items-center justify-center py-32" id="home">
-//       <div className="grid grid-cols-1 md:grid-cols-2 place-items-center px-8 md:px-0">
-//         {/* <div className="mb-8 md:mb-0 md:p-20" data-aos="fade-up">
-//             <h1 className="text-4xl lg:text-7xl uppercase text-blue mb-4 font-third">Securing and innovating indonesia</h1>
-//         </div> */}
-//         {/* <div className="relative w-full" >
-//           <HeroSlider />
-//         </div> */}
-//       </div>
-//     </section>
-//   );
-// }
-
 "use client"
 import HeroSlider from "../UI/HeroSlider";
 
 export default function Hero() {
     return (
-      <section className="relative h-screen flex items-center justify-center" id="hero">
+      <section className="relative h-screen flex items-center justify-center py-20" id="hero">
         <div className="container mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 place-items-center px-8 md:px-0">
             <div className="mb-8 md:mb-0 md:p-20" data-aos="fade-up">
-              <h1 className="text-5xl lg:text-7xl uppercase text-blue mb-4 font-third">Securing and innovating indonesia</h1>
+              <h1 className="text-4xl lg:text-7xl uppercase text-blue mb-4 font-third">Securing and innovating indonesia</h1>
             </div> 
             <div className="relative w-full" >
               <HeroSlider />
